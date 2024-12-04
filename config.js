f3f68aec1d5ceb7e5c85002e8f5f7f9fbc3de76a
Mohammad Nayan <105283967+MOHAMMAD-NAYAN@users.noreply.github.com>
2024-12-04 18:05:53 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  ownerUsernames: ["MOHAMMADNAYAN"],//Your Username
+  ownerNumber: "+8801615298449",//Your Number
+  admin: ["5920596290"], // your id
+  prefix: "/", // dont change prefix 
+  telegramBotToken: "7905674155:AAFnq9LvH1goV77-FuAyTy9LJ-7GU9xX-Wo",//Your Bot Tokens
+  imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
+  port: process.env.PORT || 8053 || 8053, // port
+  mess: {
+    ownerlink: "https://t.me/MOHAMMADNAYAN",
+    eror: "Internal Server Eror 😵",
+    owner: "Sorry, this command can only be accessed by the owner!",
+    group: "Sorry, this command can only be used within a group!",
+    wait: "Your request is being processed...",
+  },
+};
