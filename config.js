c39abbb46902f950d1f08ccf268f9a7a968d9193
Mohammad Nayan <105283967+MOHAMMAD-NAYAN@users.noreply.github.com>
2024-12-04 18:08:48 +0600

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   ownerNumber: "+8801615298449",//Your Number
   admin: ["5920596290"], // your id
   prefix: "/", // dont change prefix 
-  telegramBotToken: "7905674155:AAFnq9LvH1goV77-FuAyTy9LJ-7GU9xX-Wo",//Your Bot Tokens
+  telegramBotToken: "Your_Bot_Token",//Your Bot Tokens
   imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
   port: process.env.PORT || 8053 || 8053, // port
   mess: {
