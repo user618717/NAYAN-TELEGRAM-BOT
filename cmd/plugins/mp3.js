ff98fb28904385578a9e015b654df42f8726a14d
Mohammad Nayan <105283967+MOHAMMAD-NAYAN@users.noreply.github.com>
2024-12-14 16:05:10 +0600

    Update mp3.js

--- cmd/plugins/mp3.js
+++ cmd/plugins/mp3.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const fetch = require('node-fetch');
-const { alldown } = require('nayan-video-downloader');
+const { alldown } = require('nayan-videos-downloader');
 
 module.exports = {
   config: {
