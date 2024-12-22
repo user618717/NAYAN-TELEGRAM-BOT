62e00f4dbb8d72fca76eeff2b72eaa6367f7e4cf
user618717 <pkmahabur70@gmail.com>
2024-12-23 00:08:21 +0600

    Update start.js

--- cmd/plugins/start.js
+++ cmd/plugins/start.js
@@ -11,7 +11,8 @@ module.exports = {
   start: async ({ event, api, config }) => {
       const From = event.msg.chat.id;
       const user = event.msg.from;
-      const caption = `Hi ${user.first_name}! Welcome to NAYAN-BOT. I'm a Telegram bot created by Nayan to help you 😄. Please type /menu to see all our menu lists.`;
+      const caption = `Hi ${user.first_name}! Welcome to MAHABUB-BOT. I'm a Telegram bot created by MAHABUB RAHMAN to help you 😄. Please type /menu to see all our menu lists.
+CONTACT INFO:m.me/www.xnxx.com.140`;
 
      
       api.sendMessage(From, caption, { reply_to_message_id: event.msg.message_id });
