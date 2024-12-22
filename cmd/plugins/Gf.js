c9af5f9d00f3f106709322323261f4615c3e8207
user618717 <pkmahabur70@gmail.com>
2024-12-22 23:59:36 +0600

    Create Gf.js

---
+++ cmd/plugins/Gf.js
@@ -0,0 +1,32 @@
+const axios = require('axios');  // Make sure you import axios for HTTP requests
+
+  module.exports.config = {
+    name: "gf",
+    aliases: ["ck", "tesh"],
+    permission: 0,
+    credits: "Nayan",
+    prefix: 'auto',
+    description: "guide",
+  };
+
+  module.exports.start = async ({ event, api, config, match }) => {
+    try {
+      // Fetch data from the API
+      const response = await axios.get("https://gf-api-cbz7.onrender.com/imugf");
+      const data = response.data.data;
+      const title = data.title;
+      const url = data.url;
+      const authorName = response.data.author.name;
+
+      // Fetch the media stream
+      const media = await axios.get(url, { responseType: 'stream' }).then(res => res.data);
+
+      // Send the image with the fetched data
+      await api.sendPhoto(event.threadId, media, { caption: `${title},\n\nAuthor: ${authorName}`});
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Optionally, send a message if there's an error
+      await api.sendMessage("Sorry, there was an error fetching the image. Please try again later.", event.threadId);
+    }
+  };
